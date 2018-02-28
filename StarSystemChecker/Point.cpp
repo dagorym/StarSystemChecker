ac5c3425ecd0f01de6e8fd23d840467564d976fa
Tom Stephens <thomas.stephens@gmail.com>
2018-02-28 13:53:14 -0700

    Create skelton of Point class

---
+++ StarSystemChecker/Point.cpp
@@ -0,0 +1,18 @@
+/*
+ * Point.cpp
+ *
+ *  Created on: Feb 28, 2018
+ *      Author: tstephen
+ */
+
+#include "Point.h"
+
+Point::Point() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Point::~Point() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ StarSystemChecker/Point.h
@@ -0,0 +1,23 @@
+/*
+ * Point.h
+ *
+ *  Created on: Feb 28, 2018
+ *      Author: Tom Stephens
+ */
+
+#ifndef POINT_H_
+#define POINT_H_
+
+/**
+ * @brief Class to hold 3D point
+ *
+ * @author Tom Stephens
+ * @date Create: FEb 28, 2018
+ */
+class Point {
+public:
+	Point();
+	virtual ~Point();
+};
+
+#endif /* POINT_H_ */
