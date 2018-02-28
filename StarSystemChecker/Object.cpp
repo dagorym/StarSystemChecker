cfbc9eb86e445c76cf4a02e443998f2a36980679
Tom Stephens <thomas.stephens@gmail.com>
2018-02-28 13:46:06 -0700

    Add basic contents of Object

--- StarSystemChecker/Object.cpp
+++ StarSystemChecker/Object.cpp
@@ -8,7 +8,16 @@
 #include "Object.h"
 
 Object::Object() {
+	m_name="Unnamed";
+	m_mass = 0;
+	m_semiMajor = 0;
+	m_eccentricity = 0;
 	// TODO Auto-generated constructor stub
+
+}
+
+Object::Object(std::string name, double mass, double sm, double e):
+	m_name(name),m_mass(mass), m_semiMajor(sm), m_eccentricity(e){
 
 }
 
--- StarSystemChecker/Object.h
+++ StarSystemChecker/Object.h
@@ -8,10 +8,43 @@
 #ifndef OBJECT_H_
 #define OBJECT_H_
 
+#include <vector>
+#include <string>
+
+/**
+ * @brief This class holds the data for a single astronomical object
+ *
+ * @author Tom Stephens
+ * @date Created: Feb 28, 2018
+ */
 class Object {
 public:
 	Object();
+	Object(std::string name, double mass, double sm, double e=0);
 	virtual ~Object();
+
+	std::string getName() const { return m_name; }
+	double getSemiMajor() const { return m_semiMajor; }
+	double getMass() const { return m_mass; }
+	double getEccentricity() const { return m_eccentricity; }
+
+	void setName(std::string n) { m_name = n; }
+	void setSemiMajor(double s) { m_semiMajor = s; }
+	void setMass(double m) { m_mass = m; }
+	void setEcentricity(double e) { m_eccentricity = e; }
+
+
+private:
+	// The name of the object
+	std::string m_name;
+	// Size of the orbital semi-major axis in meters
+	double m_semiMajor;
+	// mass of the object in kilograms
+	double m_mass;
+	// orbital eccentricity
+	double m_eccentricity;
 };
+
+typedef std::vector<Object> ObjectList;
 
 #endif /* OBJECT_H_ */
