bc8c87a578ccb6599a8eef84a0f12bec2eb3549b
Tom Stephens <thomas.stephens@gmail.com>
2018-03-01 13:03:16 -0700

    Add skeleton for the Integrator class.
    This class will perform the actual n-body calculations for a given
    system and time step.

---
+++ StarSystemChecker/Integrator.cpp
@@ -0,0 +1,18 @@
+/*
+ * Integrator.cpp
+ *
+ *  Created on: Mar 1, 2018
+ *      Author: tstephen
+ */
+
+#include "Integrator.h"
+
+Integrator::Integrator() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Integrator::~Integrator() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ StarSystemChecker/Integrator.h
@@ -0,0 +1,17 @@
+/*
+ * Integrator.h
+ *
+ *  Created on: Mar 1, 2018
+ *      Author: tstephen
+ */
+
+#ifndef INTEGRATOR_H_
+#define INTEGRATOR_H_
+
+class Integrator {
+public:
+	Integrator();
+	virtual ~Integrator();
+};
+
+#endif /* INTEGRATOR_H_ */
